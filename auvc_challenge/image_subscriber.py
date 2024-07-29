25a837181824fcb28732571f55d76d215788f669
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2024-07-29 16:57:48 +0000

    use cvbridge

--- auvc_challenge/image_subscriber.py
+++ auvc_challenge/image_subscriber.py
@@ -3,6 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
 import cv2
 
@@ -10,6 +11,8 @@ import cv2
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__("image_subscriber")
+
+        self.cvb = CvBridge()
 
         self.subscription = self.create_subscription(
             Image, "bluerov2/camera", self.image_callback, 10
@@ -24,25 +27,10 @@ class ImageSubscriber(Node):
             msg (Image): The image message
         """
         # Convert Image message to OpenCV image
-        image = self.msg_to_image(msg)
+        image = self.cvb.imgmsg_to_cv2(msg)
 
         # Save the image
         cv2.imwrite("image.png", image)
-
-    def msg_to_image(self, msg: Image):
-        """
-        Convert an Image message to an OpenCV image.
-
-        Args:
-            msg (Image): The image message
-
-        Returns:
-            image (numpy.ndarray): The OpenCV image
-        """
-        # Convert ROS Image message to OpenCV image
-        image = cv2.imdecode(msg.data, cv2.IMREAD_COLOR)
-
-        return image
 
 
 def main(args=None):
